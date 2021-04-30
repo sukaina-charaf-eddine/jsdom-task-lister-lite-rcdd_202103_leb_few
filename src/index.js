c924275e374d59396d9fb05653e9f5eca428f538
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-30 10:38:29 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let button = document.querySelector("form");
+button.addEventListener("submit", append);
+let ul=document.getElementById("tasks");
+function append(e) {
+  let cal = document.getElementById("new-task-description").value;
+  let li = document.createElement("li");
+  let dis = cal;
+  li.innerHTML = dis;
+  ul.appendChild(li);
+  e.preventDefault();
+}
 });
